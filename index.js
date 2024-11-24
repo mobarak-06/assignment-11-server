517e9f420eed0d6460b7105c885e968880d19c37
Mobarak Hossain <mobarakhossain121838@gmail.com>
2024-11-24 23:58:11 +0600

    full update section added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -12,8 +12,6 @@ app.use(cors());
 // assignment-11
 // piDFjbG60mGvpC1w
 
-
-
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.dyqxkty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,62 +20,82 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
-    const blogsCollection = client.db('worldAdventure').collection('blogs');
-    const commentsCollection = client.db('worldAdventure').collection('comments');
+    const blogsCollection = client.db("worldAdventure").collection("blogs");
+    const commentsCollection = client
+      .db("worldAdventure")
+      .collection("comments");
 
-    app.get('/blogs', async(req,res) => {
-        const result = await blogsCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/blogs", async (req, res) => {
+      const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
 
-    app.get('/blogs/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)};
-        const result = await blogsCollection.findOne(query);
-        res.send(result);
-    })
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get('/addComments/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = { id: id};
-        const result = await commentsCollection.find(query).toArray();
-        res.send(result);
-    })
+    app.get("/addComments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { id: id };
+      const result = await commentsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post('/addComments', async(req, res) => {
-        const comments = req.body;
-        const result = await commentsCollection.insertOne(comments);
-        res.send(result);
-    })
+    app.post("/addComments", async (req, res) => {
+      const comments = req.body;
+      const result = await commentsCollection.insertOne(comments);
+      res.send(result);
+    });
 
-    app.post('/addBlogs', async(req, res) => {
-        const blog = req.body;
-        const result = await blogsCollection.insertOne(blog);
-        res.send(result);
-    })
+    app.post("/addBlogs", async (req, res) => {
+      const blog = req.body;
+      const result = await blogsCollection.insertOne(blog);
+      res.send(result);
+    });
+
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const blogs = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedBlog = {
+        $set: {
+          ...blogs,
+        },
+      };
+      const result = await blogsCollection.updateOne(
+        filter,
+        updatedBlog,
+        options
+      );
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 app.listen(port, () => {
-    console.log(`the server ias running on  port ${port}`);
-})
+  console.log(`the server ias running on  port ${port}`);
+});
